79a500759d1499ad0c825138817d7748585d5c35
arscool3 <45947743+arscool3@users.noreply.github.com>
2022-02-06 17:56:53 +0600

    release

---
+++ handlers.py
@@ -0,0 +1,21 @@
+import asyncio
+import json
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+file = open('response_a.json')
+json_data = json.load(file)
+booking_list = list(json_data)
+
+
+@app.post('/search/{booking_id}')
+async def search(booking_id: int):
+    await asyncio.sleep(2)
+    return booking_list[booking_id]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9001)
